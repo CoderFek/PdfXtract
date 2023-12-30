e99d57fe47d9270899e2e251aeabcfb441db760c
CoderFek <praveen.anand2212@gmail.com>
2023-12-30 19:57:40 +0530

    Second commit

--- tools/views.py
+++ tools/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UploadPdfForm
+from django.contrib import messages
+from .models import Category
+# from django.http import HttpResponseRedirect, HttpResponse
+# from django.urls import reverse
 
 # Create your views here.
 def linkview(request):
@@ -16,17 +20,21 @@ def pdfUploadView(request):
         form = UploadPdfForm(request.POST, request.FILES)
         if form.is_valid():
             category = form.cleaned_data['category']
+            catg = Category.objects.get(name = category)
             form.save()
+            
             # return HttpResponse('The file is saved!')
 
-            # category = Category.objects.get(slug = category_slug)
 
-            if category == 'Link':
-                return redirect('linkview')
-            elif category == 'Emails':
-                return redirect('emailview')
-            elif category == 'Text':
-                return redirect('textview')
+            if catg.name == 'Link':
+                messages.success(request, "Links extracted successfully!!")
+                return redirect('link')
+            elif catg.name == 'Emails':
+                messages.success(request, "Emails extracted successfully!!")
+                return redirect('emails')
+            elif catg.name == 'Text':
+                messages.success(request, "Text extracted successfully!!")
+                return redirect('text')
     else:
         form = UploadPdfForm()
     
